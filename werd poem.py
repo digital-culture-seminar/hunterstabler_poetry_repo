c3be90a67f14a59c270ada8a650a410b2726e55e
Hunter Stabler <hunterstabler@gmail.com>
2018-09-03 15:33:45 -0500

    9_03_18 poem generator_01
    increased corpus, and figured out how it works a little better. still need to create proper nouns and more categories based on grammar. :poodle:

--- werd poem.py
+++ werd poem.py
@@ -10,34 +10,54 @@ created August 28th 2018
 import random
 
 # "list of nouns"
-nouns = ["salor", "sheeple", "weavel", "goober"]
+nouns = ["savant", "sheeple", "weavel", "goober", "tuber", "owl", "bowels",
+         "carpet", "snack", "arch", "toenail", "horse hair", "compost", "casm", 
+        "pocket", "dullard", "mallard", "listicle", "miracle", "awl", 
+        "Demiurge"  ]
 
 # list of verbs
-verbs = ["flies", "whacks", "dabbles", "immolates", "tastes"]
+verbs = ["flies", "whacks", "dabbles", "immolates", "tastes", "summons",
+         "channels", ]
 
 # list of adjectives
-adjectives = ["cold", "frumpy", "acrid", "sour", "toasty", "smarmy"]
+adjectives = ["cold", "frumpy", "acrid", "sour", "toasty", "smarmy", "doty",
+              "spidery", "wispy", "angular", "sharp", "tiniest", "basic", 
+              "omnipresent", "oracular", "miraculous", "super computer", 
+              "awe inspiring"]
 
+adverbs = ["slinkily", "eagerly", "brutally", "unabashedly", "daily", "soon", 
+           "outside", "underground","prophetically", "extremely"  ]
 noun = random.choice(nouns)
+second_noun = random.choice(nouns)
+third_noun = random.choice(nouns)
 
 verb = random.choice(verbs)
+second_verb = random.choice(verbs)
 
 adjective = random.choice(adjectives)
 second_adjective = random.choice(adjectives)
+
+adverb = random.choice(adverbs)
 
 ##concatenation
 #print "The " + adjective + ", " + second_adjective + " " + noun + " " + verb
 
 #word poem
-print "The " + adjective + " " + noun + ""
-# for loop to iterate through verbs
-i = 1
-for verb in verbs:
-    whitespace = " " * i
-    print whitespace + verb
-    i = i + 1
+#print "The " + adjective + " " + noun + ""
+## for loop to iterate through verbs
+#i = 1
+#for verb in verbs:
+#    whitespace = " " * i
+#    print whitespace + verb
+#    i = i + 1
+
+
+
+
 #string formatting
-#print "The {adj} {n} {v}.".format(adj=adjective, n=noun, v=verb)
+print "The {adj} {n} {adv} {v} a {sn} whilst the {tn} {sv}.".format(adj=adjective,
+           n=noun, v=verb, sn=second_noun, tn=third_noun, sv=second_verb, 
+           adv=adverb, )
 
 #i = 0
 #for noun in nouns:
