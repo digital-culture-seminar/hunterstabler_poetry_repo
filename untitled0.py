b29215d13886d5c50c35635713f88b3f7b115a2f
Hunter Stabler <hunterstabler@gmail.com>
2018-10-11 15:40:59 -0500

    lists edited

--- untitled0.py
+++ untitled0.py
@@ -1,45 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  9 11:35:45 2018
+Created on Thu Oct 11 06:45:07 2018
 
 @author: hunterstabler
 """
 
-#new word poem with multiple corpus sources
+##CSV experiment
 
-import random
-
-
-with open ("adjective.txt") as file:
-    adj = file.read()
-    
-with open ("adverb.txt") as file:
-    adv = file.read()
-    
-with open ("article.txt") as file:
-    art = file.read()
-    
-with open ("conjunction.txt") as file:
-    conj = file.read()
-    
-with open ("interjection.txt") as file:
-    intj = file.read()
-    
-with open ("noun.txt") as file:
-    nn = file.read()
-    
-with open ("preposition.txt") as file:
-    prep = file.read()
-    
-with open ("pronoun.txt") as file:
-    pron = file.read()
-    
-with open ("verb.txt") as file:
-    vrb = file.read
-    
-    
-n = random.choice(nn)
-
-
-print n
+import csv
